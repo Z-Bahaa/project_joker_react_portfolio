0237bed693d3c032f6658c5467422a2c3da8bbab
Z-Bahaa <zizobahaapersonal@gmail.com>
2023-08-24 05:50:47 +0200

    updated resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
-import CV from '../../assets/resume.pdf'
+// import CV from '../../assets/resume.pdf'
 
 import React from 'react'
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} className='btn'>Download CV</a>
+      <a href='https://docs.google.com/document/d/1TvHfqdgGQ7ACcpUJ9I5aU83MZDZLyf0DYyjS7hmbAt0/edit?usp=sharing' className='btn'>Download CV</a>
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
   )
