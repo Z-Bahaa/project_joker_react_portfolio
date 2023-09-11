1f0a297e1ea72429357e588bfc6b9ac533dacffa
Z. Bahaa <99715468+Z-Bahaa@users.noreply.github.com>
2023-09-11 17:31:36 +0300

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,13 +5,12 @@ import {SiLeetcode} from 'react-icons/si';
 
 
 const HeaderSocials = () => {
-let s = "30px";
   return (
     <div className="header__socials">
-      <a href='https://leetcode.com/zizobahaapersonal/' target='_blank'  rel="noreferrer"><SiLeetcode size=s/></a>
-      <a href='https://www.linkedin.com/in/zizo-bahaa' target='_blank'  rel="noreferrer"><AiOutlineLinkedin size=s /></a>
-      <a href='https://github.com/Z-Bahaa' target='_blank'  rel="noreferrer"><FiGithub size=s/></a>
-      <a href='https://www.facebook.com/profile.php?id=100071918118359' target='_blank'  rel="noreferrer"><AiOutlineFacebook size=s /></a>
+      <a href='https://leetcode.com/zizobahaapersonal/' target='_blank'  rel="noreferrer"><SiLeetcode size="30px"/></a>
+      <a href='https://www.linkedin.com/in/zizo-bahaa' target='_blank'  rel="noreferrer"><AiOutlineLinkedin size="30px" /></a>
+      <a href='https://github.com/Z-Bahaa' target='_blank'  rel="noreferrer"><FiGithub size="30px"/></a>
+      <a href='https://www.facebook.com/profile.php?id=100071918118359' target='_blank'  rel="noreferrer"><AiOutlineFacebook size="30px" /></a>
     </div>
   )
 }
