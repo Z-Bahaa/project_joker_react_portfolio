3fe19a83df4ca4de08959751546e09e4e00a82d8
Z. Bahaa <99715468+Z-Bahaa@users.noreply.github.com>
2023-09-11 17:51:44 +0300

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,4 +1,4 @@
-import { AiOutlineLinkedin, AiOutlineFacebook,} from 'react-icons/ai'
+import { AiOutlineLinkedin,} from 'react-icons/ai'
 import {FiGithub} from 'react-icons/fi'
 import {SiLeetcode} from 'react-icons/si';
 
@@ -8,7 +8,8 @@ const HeaderSocials = () => {
   return (
     <div className="header__socials">
       <a href='https://leetcode.com/zizobahaapersonal/' target='_blank'  rel="noreferrer"><SiLeetcode size="30px"/></a>
-      <a href='https://www.linkedin.com/in/zizo-bahaa' target='_blank'  rel="noreferrer"><AiOutlineLinkedin size="30px" /></a>
+      <a href='https://www.linkedin.com/in/zizo-bahaa' target='_blank'  rel="noreferrer"> <AiOutlineLinkedin size="30px" /></a>
+<a href='https://github.com/Z-Bahaa' target='_blank'  rel="noreferrer"> <FiGithub size="30px" /></a>
     </div>
   )
 }
