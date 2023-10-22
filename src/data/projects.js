912ee551f1c7ec5896cbc1578e3db1f50901c392
Z-Bahaa <zizobahaapersonal@gmail.com>
2023-10-22 21:33:22 +0200

    added new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,13 +4,23 @@ import IMG4 from '../assets/portfolio4.png'
 import IMG5 from '../assets/portfolio5.jpg'
 import IMG6 from '../assets/portfolio6.png'
 import IMG7 from '../assets/IMG7.png'
+import IMG8 from '../assets/IMG8.png'
 
 const projects = [
+  {
+    id: 8,
+    img: IMG8,
+    title: 'Instalog: event log for business management',
+    tech: 'ReactJs, tailwindCSS, ViteJS',
+    github: 'https://github.com/Z-Bahaa?tab=repositories&q=instalog&type=&language=&sort=',
+    demo: 'https://instalog-exy8.onrender.com/',
+    date: 2023,
+  },
   {
     id: 7,
     img: IMG7,
     title: 'Vairu: Affordable UGC videos for all Brands',
-    tech: 'ReactJs, Vanilla CSS, ViteJS, Bubble.io',
+    tech: 'no-code development platforms, stripe',
     github: 'https://github.com/Z-Bahaa/vairu-ui.git',
     demo: 'https://vairu.app/version-test',
     date: 2023,
